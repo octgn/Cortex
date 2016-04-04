bc9b0ae148e33c4efdb833c47d6e2ebf9301e379
Kelly Elton <its.the.doc@gmail.com>
2016-04-04 17:07:34 -0400

    Start

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var bower = require('gulp-bower');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('bower', function() {
+  return bower();
+});
