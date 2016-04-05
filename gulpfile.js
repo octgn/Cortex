73bd1b74c7cb2af61b542ec3fe44399c86f95b3c
Kelly Elton <its.the.doc@gmail.com>
2016-04-04 21:12:48 -0400

    Getting folder path

--- gulpfile.js
+++ gulpfile.js
@@ -15,12 +15,13 @@ gulp.task("run", function () {
 	return gulp.src("build").pipe(runElectron());
 });
 
-gulp.task("default", ["watch", "build", "run"], function () {
+gulp.task("default", ["build", "run"], function () {
 });
 
 gulp.task('bower', function() {
 	bower();
 	gulp.src("semantic/dist/*.min.js").pipe(gulp.dest("build/lib")); 
+	gulp.src("semantic/dist/themes/**/*").pipe(gulp.dest("build/css/themes")); 
 	gulp.src("semantic/dist/*.min.css").pipe(gulp.dest("build/css")); 
 	gulp.src("bower_components/jquery/dist/*.min.js").pipe(gulp.dest("build/lib")); 
 	return gulp.src("bower_components/react/*.min.js").pipe(gulp.dest("build/lib")); 
